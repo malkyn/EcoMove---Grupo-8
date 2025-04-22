69d440aeb06a9778668f8b48b66c7942695a02f0
JoaoSantosPD <joao.correiapedro@outlook.com>
2025-04-22 19:24:53 -0300

    add campos em usuario, veiculos

--- UPX - Backend/models.py
+++ UPX - Backend/models.py
@@ -1,7 +1,17 @@
 from app import db
 
-class Usucario(db.Model):
-    id_usuario = db.Column(db.integer, primary_key=True)
+class Usuario(db.Model):
+    id_usuario = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(100), nullable=False)
+    email= db.Column(db.String(100), unique=True, nullable=False)
+    senha= db.Column(db.String(100), nullable=False)
 
 class Veiculo:
-    id_usuario = db.Column(db.integer, db.ForeingKey('Usuario'))
+    id_veiculo = db.Column(db.Integer, primary_key=True)
+    id_usuario = db.Column(db.Integer, db.ForeignKey('usuario.id_usuario'))
+    modelo = db.Column(db.String(100), nullable=False)
+    placa = db.Column(db.String(10), unique=True, nullable=False)
+    tipo = db.Column(db.String(50)) 
+
+class Carona:
+    id_carona = db.Column(db.Integer, primary_key=True)
