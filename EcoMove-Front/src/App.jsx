c83d5ff2ff27bd654771b57d3c3b0c8ee5a23de7
vinir <46539805+malkyn@users.noreply.github.com>
2025-04-23 23:19:51 -0300

    adição router

--- EcoMove-Front/src/App.jsx
+++ EcoMove-Front/src/App.jsx
@@ -1,16 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Home from'./pages/Home'
+import Usuarios from './pages/Usuarios'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <Home></Home>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/usuarios" element={<Usuarios></Usuarios>}></Route>
+      </Routes>
+    </Router>
   )
 }
 
