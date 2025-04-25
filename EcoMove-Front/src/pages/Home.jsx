1dd858dab2f765b48d556959bcb18c9e1c4541d1
Murillo024 <murilolf402@gmail.com>
2025-04-24 22:51:15 -0300

    atualizaçoes no front

--- EcoMove-Front/src/pages/Home.jsx
+++ EcoMove-Front/src/pages/Home.jsx
@@ -3,10 +3,65 @@ import './Home.css'
 function Home(){
     return(
         <>
-        <div className='container'>
-            <h1 className="home">tchau</h1>
-            <p>borrraaaa</p>
+        <header className='container'>
+            <img src="" alt="" />
+            <h1>Eco Move</h1>
+            <p>inicio</p>
+            <p>Como funciona</p>
+            <p>Cadastre-se</p>
+            <p>Entrar</p>
+        </header>
+        <div>
+            <h1>Compartilhe o caminho, conecte pessoas</h1>
+            <img src="" alt="" />
+            <p>A Eco Move é uma solução inteligente que conecta pessoas a meios de transporte sustentáveis, como bicicletas, caronas compartilhadas, veículos elétricos e rotas integradas com transporte público. Tudo isso com tecnologia,  impacto positivo para o planeta.</p>
+            <button>
+                <p>Cadastres-se Agora</p>
+            </button>
+            <button>
+                <p>Como funciona</p>
+            </button>
         </div>
+        <div className='funcionabilidades'>
+            <h1>Principais funcionabilidades</h1>
+            <p>Nossa plataforma foi desenvolvida para tornar o compartilhamento de caronas uma
+            experiência simples, segura e agradável.</p>
+            <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+            <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+            <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+            <link rel="shortcut icon" href="" type="image/x-icon" />
+            <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+            <link rel="shortcut icon" href="
+            " type="image/x-icon" />
+            <h2>️ Rotas Sustentáveis Inteligentes </h2>
+            <p>Motoristas e passageiros se cadastram
+rapidamente, com validação de informações
+para garantir mais segurança.</p>
+            <h2>Localização de Estações de Recarga</h2>
+            <p>Visualize e publique viagens facilmente.
+             Encontre opções com base na origem e destino.</p>
+            <h2> Caronas Compartilhadas e Seguras</h2>
+            <p>Conexão rápida entre usuários por meio de
+grupos regionais, organizando caronas em
+tempo real.</p>
+            <h2>Monitoramento do Impacto Ambiental</h2>
+            <p>Após cada viagem, motoristas e passageiros
+podem avaliar a experiência, aumentando a
+confiabilidade.</p>
+            <h2>Sistema de Recompensas Ecológicas</h2>
+            <p>Verificação de identidade e normas claras para
+            uma comunidade respeitosa e confiável.</p>
+            <h2> Interface Intuitiva e Acessível</h2>
+            <p>Verificação de identidade e normas claras para
+            uma comunidade respeitosa e confiável.</p>
+        </div>
+        <div className='comofunciona'>
+
+        </div>
+
+
+    
+        
         </>
     )
 }
