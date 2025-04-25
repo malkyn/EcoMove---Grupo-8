590e9d2c4ed6908c00847169efcf919d7bfa0459
fernandogabriel2005 <fernando1234camaro@gmail.com>
2025-04-25 13:35:27 -0300

    Feita boa parte do cabeçalho(Header) da página Usuários

--- EcoMove-Front/src/pages/Usuarios.jsx
+++ EcoMove-Front/src/pages/Usuarios.jsx
@@ -1,14 +1,46 @@
-import './Usuarios.css'
+import React from "react";
+import "./Header.css";
+import Logo from "./icons/folha.svg";
+function Header() {
+  return (
+    <header className="header">
+      <div className="header-top">
+        <div className="logo">
+          <img src={Logo} className="icon-img" title="EcoMove Logo" />
 
-function Usuarios(){
-    return(
-        <>
-        <div className='container'>
-              <h1>tchau</h1>
+          <span className="logo-text">Eco Move</span>
         </div>
-        
-        </>
-    )
+
+        <nav className="menu">
+          <ul>
+            <li>
+              <a href="">Início</a>
+            </li>
+
+            <li>
+              <a href="">Como Funciona</a>
+            </li>
+
+            <li>
+              <a href="">Cadastre-se</a>
+            </li>
+
+            <li className="menu-login">
+              <a href="">Entrar</a>
+              <img src="" alt="Ícone de usuário" className="icon-user" />
+            </li>
+          </ul>
+        </nav>
+      </div>
+
+      <div className="header-bottom">
+        <img src="" alt="Voltar" className="icon-back" />
+        <a href="" className="back-link">
+          Voltar para o início
+        </a>
+      </div>
+    </header>
+  );
 }
 
-export default Usuarios
+export default Header;
