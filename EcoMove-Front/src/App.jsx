001e909098ab5283021d6cf9e7e1625341750b45
fernandogabriel2005 <fernando1234camaro@gmail.com>
2025-04-29 22:21:21 -0300

    Criando pag login

--- EcoMove-Front/src/App.jsx
+++ EcoMove-Front/src/App.jsx
@@ -6,6 +6,7 @@ import Usuarios from "./pages/Usuarios";
 import NavBar from "./components/NavBar/NavBar.jsx";
 import Footer from "./components/Footer/Footer.jsx";
 import CadastroUsuario from "./pages/CadastroUsuario.jsx";
+import Login from "./pages/Login.jsx";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -16,7 +17,11 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/usuarios" element={<Usuarios></Usuarios>}></Route>
-        <Route path="/cadastrousuario" element={<CadastroUsuario></CadastroUsuario>}></Route>
+        <Route
+          path="/cadastrousuario"
+          element={<CadastroUsuario></CadastroUsuario>}
+        ></Route>
+        <Route path="/loginForm" element={<Login></Login>}></Route>
       </Routes>
       <Footer />
     </Router>
---
+++ EcoMove-Front/src/pages/Login.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Login.css";
+import { Link } from "react-router-dom";
+
+function login() {
+  <div>
+    <h1>OI</h1>
+  </div>;
+}
+
+export default login;
