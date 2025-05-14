87ac02edbb7149a7979dd30c3021d0f067202f58
Murillo024 <murilolf402@gmail.com>
2025-05-13 23:32:38 -0300

    alteraçoes paginas usuarios/entrar

--- EcoMove-Front/src/pages/Usuarios.jsx
+++ EcoMove-Front/src/pages/Usuarios.jsx
@@ -1,9 +1,34 @@
 import React from "react";
+import "./Usuarios.css";
+import usuario2 from "./icons/usuario2.svg";
+import cadeado from "./icons/cadeado.svg";
 function Usuarios() {
   return (
-    <div>
-      
-    </div>
+    <body>
+      <div className="container">
+        <form action="">
+          <h1>Entrar na plataforma</h1>
+          <div>
+            <input placeholder="Email" type="email" required/>
+             <img src={usuario2} alt="icone de usuario" />
+          </div>
+          <div>
+            <input placeholder="Senha" type="password" required/>
+             <img src={cadeado} alt="icone de usuario" />
+             <a href="#">Esqueci minha senha</a>
+          </div>
+          
+          <button type="submit">Entrar</button>
+
+          <div>
+            <p> Não está cadastrado ? <a href="#">Cadastrar</a> </p> 
+          </div>
+          
+
+        </form>
+      </div>
+    </body>
+    
 
   );
 }
