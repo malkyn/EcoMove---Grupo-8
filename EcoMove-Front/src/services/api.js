bb59b0916d84bfd11bc874e2b2a0d4fb47473de1
Murillo dos Santos Freitas <murilolf402@gmail.com>
2026-09-10 16:18:54 -0300

    Configura URL da API por variavel de ambiente

--- EcoMove-Front/src/services/api.js
+++ EcoMove-Front/src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:5000",
-  withCredentials: true, // importante para CORS com sessão/cookies
+  baseURL: import.meta.env.VITE_API_URL || "http://127.0.0.1:5000",
+  timeout: 10000,
 });
 
 export default api;
